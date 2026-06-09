d6f12c9c5357bac07ff0357e71f35e541a8775cc
andricja <jareljambalos@gmail.com>
2026-06-09 18:14:11 +0800

    Fix ManageAdsTemp JSX structure

--- frontend_capstone/src/pages/admin/ManageAdsTemp.jsx
+++ frontend_capstone/src/pages/admin/ManageAdsTemp.jsx
@@ -215,11 +215,6 @@ function ManageAds() {
           ))
         )}
       </div>
-    </div>
-  );
-}
-
-export default ManageAds;
 
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
